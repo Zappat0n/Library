ce78df02027ff599f5a17c29110d682398981b74
angel <albarros1973@gmail.com>
2021-02-08 09:05:00 +0100

    Refactor Library

--- assets/script.js
+++ assets/script.js
@@ -8,8 +8,6 @@ function getSavedLibrary() {
   }
 }
 
-const myLibrary = getSavedLibrary();
-
 function Book(title, author, pages, status = false) {
   this.title = title;
   this.author = author;
@@ -17,97 +15,104 @@ function Book(title, author, pages, status = false) {
   this.status = status;
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
-}
+const libraryModule = ((library) => {
+  const addBook = (book) => {
+    library.push(book);
+  };
 
-function addLibraryToLocalStorage() {
-  localStorage.setItem('library', JSON.stringify(myLibrary));
-}
+  const changeBookStatus = (index) => {
+    library[index].status = !library[index].status;
+  };
 
-function removeBookFromLibrary(index) {
-  myLibrary.splice(index, 1);
-}
-
-function changeStatusToBook(index) {
-  myLibrary[index].status = !myLibrary[index].status;
-}
-
-function displayLibrary(library) {
-  const bookBody = document.querySelector('#books_body');
-  bookBody.innerHTML = '';
-
-  library.forEach((book, index) => {
-    const tr = document.createElement('tr');
-
-    tr.innerHTML = `
-      <td>${book.title}</td>
-      <td>${book.author}</td>
-      <td>${book.pages}</td>
-      <td>${book.status ? 'Read already' : 'Not yet'}</td>
-    `;
-    bookBody.appendChild(tr);
-
-    let td = document.createElement('td');
+  const createButton = (tr, index, _classList, _textContent) => {
+    const td = document.createElement('td');
     const checkBtn = document.createElement('button');
-    checkBtn.classList.add('change_status_button');
+    checkBtn.classList.add(_classList);
     checkBtn.setAttribute('data-attribute', index);
-    checkBtn.textContent = book.status ? 'Unread it?' : 'Read it?';
+    checkBtn.textContent = _textContent;
     td.appendChild(checkBtn);
     tr.appendChild(td);
-    checkBtn.addEventListener('click', (event) => {
-      const index = event.target.getAttribute('data-attribute');
-      changeStatusToBook(index);
-      addLibraryToLocalStorage();
-      displayLibrary(myLibrary);
-    });
+    return checkBtn;
+  };
 
-    td = document.createElement('td');
-    const rmBtn = document.createElement('button');
-    rmBtn.classList.add('remove_button');
-    rmBtn.setAttribute('data-attribute', index);
-    rmBtn.textContent = 'remove';
-    td.appendChild(rmBtn);
-    tr.appendChild(td);
-    rmBtn.addEventListener('click', (event) => {
-      const index = event.target.getAttribute('data-attribute');
-      removeBookFromLibrary(index);
-      addLibraryToLocalStorage();
-      displayLibrary(myLibrary);
+  const removeBook = (index) => {
+    library.splice(index, 1);
+  };
+
+  const toLocalStorage = () => {
+    localStorage.setItem('library', JSON.stringify(library));
+  };
+
+  const display = () => {
+    const bookBody = document.querySelector('#books_body');
+    bookBody.innerHTML = '';
+
+    library.forEach((book, index) => {
+      const tr = document.createElement('tr');
+      tr.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.pages}</td>
+        <td>${book.status ? 'Read already' : 'Not yet'}</td>
+      `;
+      bookBody.appendChild(tr);
+      const checkBtn = createButton(tr, index, 'change_status_button', (book.status ? 'Unread it?' : 'Read it?'));
+      const rmBtn = createButton(tr, index, 'remove_button', 'remove');
+      checkBtn.addEventListener('click', (event) => {
+        const index = event.target.getAttribute('data-attribute');
+        changeBookStatus(index);
+        toLocalStorage();
+        display();
+      });
+      rmBtn.addEventListener('click', (event) => {
+        const index = event.target.getAttribute('data-attribute');
+        removeBook(index);
+        toLocalStorage();
+        display();
+      });
     });
+  };
+
+  return { addBook, display, toLocalStorage };
+})(getSavedLibrary());
+
+const formModule = ((library) => {
+  const form = document.getElementById('book_form');
+
+  function setValues(book, el) {
+    book.title = el.title.value;
+    book.author = el.author.value;
+    book.pages = el.pages.value;
+    book.status = el.status.checked;
+  }
+
+  function clearValues(el) {
+    el.title.value = '';
+    el.author.value = '';
+    el.pages.value = '';
+    el.status.checked = false;
+  }
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const book = new Book();
+    setValues(book, event.target.elements);
+    library.addBook(book);
+    library.toLocalStorage();
+    library.display();
+    clearValues(event.target.elements);
   });
-}
 
-const newBookButton = document.getElementById('new_book_button');
-const addBookForm = document.getElementById('book_form');
+  return { form };
+})(libraryModule);
 
-newBookButton.addEventListener('click', () => {
-  addBookForm.style.display = addBookForm.style.display === 'none' ? 'block' : 'none';
-});
-
-function setValues(book, el) {
-  book.title = el.title.value;
-  book.author = el.author.value;
-  book.pages = el.pages.value;
-  book.status = el.status.checked;
-}
-
-function clearValues(el) {
-  el.title.value = '';
-  el.author.value = '';
-  el.pages.value = '';
-  el.status.checked = false;
-}
-
-addBookForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const book = new Book();
-  setValues(book, event.target.elements);
-  addBookToLibrary(book);
-  addLibraryToLocalStorage();
-  displayLibrary(myLibrary);
-  clearValues(event.target.elements);
-});
+const bookButton = (form) => {
+  const button = document.getElementById('new_book_button');
+  button.addEventListener('click', () => {
+    form.style.display = form.style.display === 'none' ? 'block' : 'none';
+  });
+};
 
 // RUNNING CODE
-displayLibrary(myLibrary);
+libraryModule.display();
+bookButton(formModule.form);
